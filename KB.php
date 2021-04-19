7a0c21cc47f6d87f30c9790589ea91ac8165a175
Reza <73782101+dragonshck@users.noreply.github.com>
2021-04-19 23:33:37 +0700

    Testing Met
    Right?

--- KB.php
+++ KB.php
@@ -9,6 +9,7 @@
     <h1> Menghitung Pecahan Uang </h1>
     <h1>Pecahan Uang</h1>
     <p>Menghitung Pecahan Uang di PHP</p>
+    Pushing Origin to Git
     <form method = "post">
     Masukan jumlah uang <input type= "text" name="uang"><br><br>
     <input type= "submit" name="submit" value= "proses">
